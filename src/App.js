0e468791f415a18ece4b15a1ae9826c0c0807c2b
Roshan J <devenforced@gmail.com>
2021-08-01 19:07:42 +0530

    refactor:adding links

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
-var emojiDictionary={
-  "👨🏻‍⚖️":"Judge",
-  "👩🏻‍🔬":"Scientist",
-  "👮🏻‍♀️":"Officer",
-  "👩🏻‍⚕️":"Doctor",
-  "👩🏻‍✈️":"Pilot",
-  "👩🏻‍🚒":"Firefighter",
-  "👨🏻‍🎤":"Singer",
-  "👨🏻‍💼":"Businessman",
-  "🕵🏻‍♀️":"Detective",
-  "👩🏻‍🚀":"Astronaut",
-  "👨🏻‍🍳":"Chef",
-  "👨🏻‍🎨":"Painter",
-}
-var knownEmojis=Object.keys(emojiDictionary);
+var emojiDictionary = {
+  "👨🏻‍⚖️": "Judge",
+  "👩🏻‍🔬": "Scientist",
+  "👮🏻‍♀️": "Officer",
+  "👩🏻‍⚕️": "Doctor",
+  "👩🏻‍✈️": "Pilot",
+  "👩🏻‍🚒": "Firefighter",
+  "👨🏻‍🎤": "Singer",
+  "👨🏻‍💼": "Businessman",
+  "🕵🏻‍♀️": "Detective",
+  "👩🏻‍🚀": "Astronaut",
+  "👨🏻‍🍳": "Chef",
+  "👨🏻‍🎨": "Painter"
+};
+var knownEmojis = Object.keys(emojiDictionary);
 
 export default function App() {
-  const [prof,setProf]=useState("");
-  const [meaning,setMeaning]=useState("");
-  function inputChangeHandler(event){
-    var newProf=event.target.value;
+  const [prof, setProf] = useState("");
+  const [meaning, setMeaning] = useState("");
+  function inputChangeHandler(event) {
+    var newProf = event.target.value;
     setProf(newProf);
-    if(newProf in emojiDictionary){
-    var newMeaning=emojiDictionary[newProf];
-  }else{
-    newMeaning="Not Available";
+    if (newProf in emojiDictionary) {
+      var newMeaning = emojiDictionary[newProf];
+    } else {
+      newMeaning = "Not Available";
+    }
+    setMeaning(newMeaning);
   }
-  setMeaning(newMeaning);
-  }
-  function emojiClickHandler(emoji){
+  function emojiClickHandler(emoji) {
     setProf(emoji);
-    var newMeaning=emojiDictionary[emoji];
+    var newMeaning = emojiDictionary[emoji];
     setMeaning(newMeaning);
   }
   return (
@@ -40,8 +40,13 @@ export default function App() {
       <nav className="navigation container">
         <div className="nav-items">
           <div className="nav-repo">
-            <a className="nav-link" href="https://github.com/dev-enforced/" target="_blank" rel="noopener noreferrer">
-                <i className="bx bxl-github"></i>
+            <a
+              className="nav-link"
+              href="https://github.com/dev-enforced/profession-emoji-check"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="bx bxl-github"></i>
             </a>
           </div>
           <h1 className="nav-title">PROFESSION CHECK ✔</h1>
@@ -56,22 +61,38 @@ export default function App() {
           <div className="main-input">
             <p>
               <label htmlFor="Emoji input">INPUT👇🏻</label>
-              <input onChange={inputChangeHandler} id="inp" type="text" placeholder="Enter your emoji here"></input>
+              <input
+                onChange={inputChangeHandler}
+                id="inp"
+                type="text"
+                placeholder="Enter your emoji here"
+              ></input>
             </p>
           </div>
           <div className="main-output">
-                <h2>Emoji Entered:<br/>{prof}</h2>
-                <br />
-                <h2>Meaning:<br/>{meaning}</h2>
+            <h2>
+              Emoji Entered:
+              <br />
+              {prof}
+            </h2>
+            <br />
+            <h2>
+              Meaning:
+              <br />
+              {meaning}
+            </h2>
           </div>
           <div className="main-options">
-              {
-                  knownEmojis.map((emoji)=>{
-                  return (
-                  <span className="main-card" onClick={()=>emojiClickHandler(emoji)}>{emoji}</span>
-                  )
-                  })
-              }
+            {knownEmojis.map((emoji) => {
+              return (
+                <span
+                  className="main-card"
+                  onClick={() => emojiClickHandler(emoji)}
+                >
+                  {emoji}
+                </span>
+              );
+            })}
           </div>
         </div>
       </div>
@@ -82,18 +103,27 @@ export default function App() {
         </div>
         <ul className="footer-socials">
           <li className="footer-inline">
-            <a className="footer-links" href="https://github.com/dev-enforced" target="_blank" rel="noopener noreferrer">
+            <a
+              className="footer-links"
+              href="https://github.com/dev-enforced"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="bx bxl-github"></i>
             </a>
           </li>
           <li className="footer-inline">
-            <a className="footer-links" href="https://twitter.com/devenforced" target="_blank" rel="noopener noreferrer">
+            <a
+              className="footer-links"
+              href="https://twitter.com/devenforced"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="bx bxl-twitter"></i>
             </a>
           </li>
         </ul>
       </footer>
-      
     </div>
   );
 }
